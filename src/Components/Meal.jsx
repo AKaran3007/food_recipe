8892e7b82f650168a23ab07fb4af3788e502cf6f
karan <akaran3007@gmail.com>
2022-12-21 11:56:27 +0530

    recipe

---
+++ src/Components/Meal.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import MealItem from "./MealItem";
+import ReacipeIndex from "./RecipeIndex";
+const Meal = () => {
+    const [search,setSearch]=useState();
+    const [show,setShow]=useState(false);
+    const [item,setItem]=useState("");
+    const [url,setUrl]=useState("https://www.themealdb.com/api/json/v1/1/search.php?f=a");
+   
+    useEffect(()=>{
+        fetch(url).then(res=>res.json()).then(data=> {
+            setItem(data.meals);
+            setShow(true);
+        })
+     },[url])
+
+     const searchRecipe=(evt)=>{
+         setUrl(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`)
+    }
+    const setIndex=(alpha)=>{
+        setUrl(`https://www.themealdb.com/api/json/v1/1/search.php?f=${alpha}`);
+    }
+return (
+    <>
+        <div className="main">
+            <div className="heading">
+                <h1>Recipe On Apps</h1>
+                <h4>Dear Friends you can search Your Recipe By Name and You will be find the Ingridiens And Instruction Of the Recipe To Make And You'll Find The Video Of The Searched Recipe To Make.</h4>
+            </div>
+            <div className="searchBox">
+                <input type="search" className="search-bar" placeholder="Search Your Food" onChange={e=> setSearch(e.target.value)} onKeyPress={searchRecipe}/>
+            </div>
+            <div className="container">
+                {
+                    show ?<MealItem data={item} /> :"Not Found"
+                
+                }
+            </div>
+            <div className="indexContainer">
+                 <ReacipeIndex alphaIndex={(alpha)=>setIndex(alpha)}/>
+            </div>
+            
+        </div>
+    </>
+)
+}
+export default Meal;
